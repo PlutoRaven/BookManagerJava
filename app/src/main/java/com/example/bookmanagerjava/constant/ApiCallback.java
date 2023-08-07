a9fa716bdeac2562e0f1d8968e44b2d643356a22
nguyenweareloup <85077437+PlutoRaven@users.noreply.github.com>
2023-08-07 23:44:55 +0700

    update

--- app/src/main/java/com/example/bookmanagerjava/constant/ApiCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanagerjava.constant;
-
-public interface ApiCallback<T> {
-    void onSuccess(T data);
-
-    void onError(Throwable t);
-}
--- app/src/main/java/com/example/bookmanagerjava/constant/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookmanagerjava.constant;
-
-public interface AppConstant {
-    String BASE = "http://192.168.167.106:8080";
-    String BASE_URL = BASE + "/api/";
-    String LOGIN_USERNAME = "LOGIN_USERNAME";
-    String LOGIN_PASSWORD = "LOGIN_PASSWORD";
-    String LOGIN_SAVE = "LOGIN_SAVE";
-}
